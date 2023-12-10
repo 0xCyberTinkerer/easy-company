12eef6e6f6e298e8e33d4cfdf1621d1499cf01f9
0xCyberTinkerer <86061767+0xCyberTinkerer@users.noreply.github.com>
2023-12-10 19:44:40 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from pymem import *
+from pymem.process import *
+from graph import janela
+
+pm = pymem.Pymem("Lethal Company.exe")
+
+gameModule = module_from_name(pm.process_handle, "UnityPlayer.dll").lpBaseOfDll
+
+def GetPtrAddr(base, offsets):
+    addr = pm.read_longlong(base)
+    for i in offsets:
+        if i !=offsets[-1]:
+            addr = pm.read_longlong(addr + i)
+    return addr + offsets[-1]
+
+while True:
+    #pm.write_int(GetPtrAddr(gameModule + 0x01BE9D00, [0x1A8, 0x198, 0x1B8, 0xB0, 0x1F4]), 2000)#dinheiro
+    #pm.write_float(GetPtrAddr(gameModule + 0x01CCC2D8, [0xD0, 0x8, 0x68, 0x10, 0xF8, 0x418]), 4.99999905)#velo default=4.99999905
+    #pm.write_float(GetPtrAddr(gameModule + 0x01CCC2D8, [0xD0, 0x8, 0x68, 0x10, 0xF8, 0x440]), 1.)#estamina default 1.
+    #pm.write_int(GetPtrAddr(gameModule + 0x01CCC2D8, [0xD0, 0x8, 0x68, 0x10, 0xF8, 0x59C]), 100) #health default 100.
+    #pm.write_int(GetPtrAddr(gameModule + 0x01CCC2D8, [0xD0, 0x8, 0x68, 0x10, 0xF8, 0x5DC]), 3) #throw default 3.
